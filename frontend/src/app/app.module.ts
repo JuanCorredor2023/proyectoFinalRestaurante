a278fc3d3d55ca5ed4f3871a13c913f57667b54e
Cesar Santamaria <cesar-santamaria@outlook.com>
2021-10-05 20:32:31 -0500

    fix conflicts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MenuComponent } from './components/menu/menu.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { NavbarComponent } from './tools/navbar/navbar.component';
 import { FooterComponent } from './tools/footer/footer.component';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
@@ -31,9 +30,7 @@ import { UseradminComponent } from './components/useradmin/useradmin.component';
 
 
 
-=======
 import { NavbarHomeComponent } from './tools/navbar-home/navbar-home.component';
->>>>>>> cesar
 
 @NgModule({
   declarations: [
@@ -44,13 +41,10 @@ import { NavbarHomeComponent } from './tools/navbar-home/navbar-home.component';
     ContactUsComponent,
     NavbarComponent,
     FooterComponent,
-<<<<<<< HEAD
     CommentComponent,
     UseradminComponent,
    
-=======
     NavbarHomeComponent
->>>>>>> cesar
   ],
   imports: [
     BrowserModule,
